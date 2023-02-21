5a9475fb71e36cbf872f15674c3884e42267da16
meimeisya <122343526+meimeisya@users.noreply.github.com>
2023-02-21 15:03:49 +0700

    Add files via upload

---
+++ pbo.py
@@ -0,0 +1,40 @@
+class segitiga:
+    def __init__(self, alas ,tinggi):
+        self.alas = alas
+        self.tinggi = tinggi
+
+    def get_luas(self):
+        return 0.5 *self.alas * self.tinggi
+segitiga1 = segitiga(5,10)
+segitiga2 = segitiga(5,10)
+segitiga3= segitiga(5,10)
+print("luas segitiga1:",segitiga1.get_luas())
+print("luas segitiga1:",segitiga2.get_luas())
+print("luas segitiga1:",segitiga3.get_luas())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ procedural.py
@@ -0,0 +1,7 @@
+def hitung_luas():
+    alas = float(input("Masukan alas: "))
+    tinggi = float(input("Masukan tinggi: "))
+    print("luas =",0.5 * alas * tinggi)
+hitung_luas()
+hitung_luas()
+hitung_luas()
